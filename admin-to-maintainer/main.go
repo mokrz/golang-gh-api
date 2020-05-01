be59e4e990bbd00920f602c58a4c596193734232
mokrz <mokrz@github.com>
2020-05-01 12:25:52 -0700

    Define `ListOrganizationRepositories`; calls https://developer.github.com/v3/repos/#list-organization-repositories

--- admin-to-maintainer/main.go
+++ admin-to-maintainer/main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/mokrz/golang-gh-api/admin-to-maintainer/types"
 )
+
+// ListOrganizationRepositories returns a list of repositories in the given organization
+func (c GitHubAPIClient) ListOrganizationRepositories(organization string) ([]types.Repository, error) {
+	req, newReqErr := http.NewRequest("GET", c.BaseURL+"orgs/"+organization+"/repos", nil)
+
+	if newReqErr != nil {
+		return nil, errors.New("ListOrganizationRepositories: http.NewRequest failed with error: " + newReqErr.Error())
+	}
+
+	res, doErr := c.HTTPClient.Do(req)
+
+	if doErr != nil {
+		return nil, errors.New("ListOrganizationRepositories: HTTPClient.Do failed with error: " + doErr.Error())
+	}
+
+	var repos []types.Repository
+	decodeErr := json.NewDecoder(res.Body).Decode(&repos)
+
+	if decodeErr != nil {
+		return nil, errors.New("ListOrganizationRepositories: Decoder.Decode failed with error: " + decodeErr.Error())
+	}
+
+	return repos, nil
+}
 
 // GitHubAPIClient allows us share resources between API calls.
 type GitHubAPIClient struct {
@@ -28,20 +57,31 @@ func (rt GitHubRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) 
 }
 
 func main() {
-	_ = "mokrz-enterprise-testing"
-	token := "asdf"
+	org := "mokrz-enterprise-testing"
+	token := "93200fc523d5a5206192d731c260bc6e4d95f21a"
 
-	_ = GitHubAPIClient{
+	ghClient := GitHubAPIClient{
 		HTTPClient: &http.Client{
 			Timeout: time.Second * 10,
 			Transport: GitHubRoundTripper{
 				NextMiddleWare: http.DefaultTransport,
 				Headers: map[string]string{
-					"Authorization": "token " + token,
+					"Authorization": "Bearer " + token,
 					"Accept":        "application/vnd.github.v3+json",
 				},
 			},
 		},
-		BaseURL: "https://api.github.com/v3/",
+		BaseURL: "https://api.github.com/",
+	}
+
+	repos, listReposErr := ghClient.ListOrganizationRepositories(org)
+
+	if listReposErr != nil {
+		fmt.Printf("Failed to list repos for org %s with error: %s\n", org, listReposErr.Error())
+		return
+	}
+
+	for _, repo := range repos {
+		fmt.Println("Got repo: " + repo.FullName)
 	}
 }
